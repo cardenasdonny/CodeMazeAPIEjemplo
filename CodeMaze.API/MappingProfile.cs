24fdebd7e948e574b371bf0aa9954e5b829b9de4
Donny Cárdenas <cardenasdonny@gmail.com>
2022-06-13 14:26:10 -0500

    ok

--- CodeMaze.API/MappingProfile.cs
+++ CodeMaze.API/MappingProfile.cs
@@ -12,9 +12,9 @@ namespace CodeMaze.API
                     opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
             CreateMap<Employee, EmployeeDto>();
-
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
+            CreateMap<EmployeeForUpdateDto, Employee>();
         }
         
     }
